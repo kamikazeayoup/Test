5573b7c3d10e434ebfe596dd90616852c635eee6
Mohamed Osama Ayoyub <mohamedosamaayoup202676@gmail.com>
2023-10-31 23:01:30 +0200

    uplod my first file

---
+++ WeatherForcastService.cs
@@ -0,0 +1,33 @@
+﻿using Assets.BusinessLayer;
+using Assets.DataAccessLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.BusinessLayer.Services
+{
+    
+   
+    public class WeatherForcastService : IWeatherForcastService
+    {
+
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+        public IEnumerable<WeatherForecast> Get()
+        {
+
+
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+                        .ToArray();
+        }
+    }
+}
